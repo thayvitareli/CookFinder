543cbd4e43863ad56286648967e9bd77d7bc46b9
thayvitareli <thainyvitareli@hotmail.com>
2025-02-23 14:16:59 -0300

    feat: list meals and view a recipient

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,21 +9,19 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
-  // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
+// export const unstable_settings = {
+//   // Ensure that reloading on `/modal` keeps a back button present.
+//   initialRouteName: '/home',
+// };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('@/assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
 
@@ -50,9 +48,8 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+      <Stack screenOptions={{ headerShown: false}}>
+      <Stack.Screen name="index" />
       </Stack>
     </ThemeProvider>
   );
---
+++ src/app/meal/[id].tsx
@@ -0,0 +1,111 @@
+import { iRecipiet } from "@/interfaces/meal.interface";
+import axios, { AxiosResponse } from "axios";
+import { Link, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { Text, View, StyleSheet, ActivityIndicator, ScrollView ,Image,Button, Linking, Touchable, TouchableOpacity} from "react-native";
+
+export default function Meal(){
+    const [recipient, setRecipient] = useState({} as iRecipiet)
+    const [isLoading, setIsLoading] = useState(true)
+
+    const {id} = useLocalSearchParams()
+
+    useEffect(() => { getRecipient()},[id])
+
+
+    const getRecipient = async()=> {
+        try{
+              console.log('buscando')
+            const {data} = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`) as AxiosResponse<{meals: iRecipiet[]}>
+console.log('data', data)
+            if(data)  setRecipient(data?.meals[0])
+
+        }catch(error:any) {
+                console.log(error)
+        }finally{
+            console.log('finalizado')
+setIsLoading(false)
+        }
+    }
+
+    return <View style={styles.container}>
+    
+    <Link href={'/'} style={styles.backButton}>
+    
+        <Text>Voltar</Text>
+    </Link>
+
+        {isLoading && <ActivityIndicator />}
+
+  
+    <ScrollView contentContainerStyle={styles.scroll} showsVerticalScrollIndicator={false}>
+      <Text style={styles.title}>{recipient.strMeal}</Text>
+      <Image source={{ uri: recipient.strMealThumb }} style={{ width: '100%', height: 200, borderRadius: 10 }} />
+      <Text style={{ marginTop: 20, fontSize: 18, fontWeight: 'bold' }}>Instruções:</Text>
+      <Text style={{ fontSize: 16, marginTop: 10 }}>{recipient.strInstructions}</Text>
+      {recipient.strYoutube && (
+        <TouchableOpacity 
+        style={styles.button}
+        onPress={() => Linking.openURL(recipient.strYoutube)} >
+            <Text style={styles.buttonTitle}>Assistir no YouTube</Text>
+
+        </TouchableOpacity>
+        
+      )}
+    </ScrollView>
+
+
+    </View>
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        padding: 24,
+        backgroundColor:"#e2e2e2e2",
+        alignItems:'center',
+
+    },
+    backButton: {
+        padding:5,
+        alignSelf:'flex-start',
+        
+    
+    },
+    scroll:{
+        flexGrow:1,
+        padding:10,
+        gap:10,
+    },
+    loading: {
+        marginTop:30,
+    },
+    heading: {
+       fontSize:24,
+       fontWeight: 'bold'
+    },
+    title: {
+        textAlign:'center',
+    fontSize: 24, fontWeight: 'bold' 
+    },
+  
+   image: {
+        width: '100%',
+        height:200,
+        borderTopRightRadius:20,
+        borderTopLeftRadius:20,
+        objectFit:'cover'
+    },
+    button: {
+        alignItems:'center',
+      
+        padding:10,
+        borderRadius: 10,
+        backgroundColor: "#d57070"
+    },
+    buttonTitle: {
+        color:'#fff',
+        fontWeight:'600'
+    }
+})
